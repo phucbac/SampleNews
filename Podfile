3810d80f1901feab2338dd59835acc9d8da27c25
Phucbac <phamphucbac1705@gmail.com>
2019-12-16 09:39:03 +0700

    Realm Pod Install

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'SampleNew' do
+  pod 'RealmSwift'
+end
---
+++ Pods/Target Support Files/Pods-SampleNew/Pods-SampleNew-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleNew : NSObject
+@end
+@implementation PodsDummy_Pods_SampleNew
+@end
